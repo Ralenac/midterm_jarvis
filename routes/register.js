5b8f751e359a4aba9830946683a6c72b15dc661f
Gagandeep Uppal <guppal.profile@gmail.com>
2021-10-20 14:42:46 +0000

    views for login page

---
+++ routes/register.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+
+  router.get("/register", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const users = data.rows;
+        let templateVars = { user: req.session.user };
+        res.render( "register", templateVars);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+
+  /* GET Signup */
+  router.get('/signup', function(req, res) {
+    res.render('register', { title: 'Signup Page'});
+});
+
+}
+//registering new user
+app.get('/register', (req, res) => {
+  const userID = req.session.user_id;
+  if (userID) {
+    return res.redirect("/urls");
+  }
+  const templateVars = { user: users[req.session.user_id] };
+  res.render('register', templateVars);
+});
+app.get("/register", (req, res) => {
+  let ejsVars = { user: req.session.user };
+  res.render("register", ejsVars);
+});
